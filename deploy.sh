9a22234eb7d5f12893d73865b84e21f73d2d3211
poskon <poskon@gmail.com>
2023-08-26 14:55:13 +0300

    delete sudo for startup-script.ymal & deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /home/yc-user/
-sudo git clone -b monolith https://github.com/express42/reddit.git
-cd /home/yc-user/reddit && sudo bundle install
-sudo puma -d
+git clone -b monolith https://github.com/express42/reddit.git
+cd /home/yc-user/reddit && bundle install
+puma -d
